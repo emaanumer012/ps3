085a29f52d3d4b3c56775e1f05091179497a9e74
abubakar-javed <abubakarjaved03145005454@gmail.com>
2023-12-01 19:33:35 +0500

    Initial aligned code with main

--- ps3/src/expressivo/Expression.java
+++ ps3/src/expressivo/Expression.java
@@ -3,6 +3,18 @@
  */
 package expressivo;
 
+import java.util.Map;
+
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.TokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
+import expressivo.parser.ExpressionLexer;
+import expressivo.parser.ExpressionParser;
+
 /**
  * An immutable data type representing a polynomial expression of:
  *   + and *
@@ -17,16 +29,39 @@ package expressivo;
 public interface Expression {
     
     // Datatype definition
-    //   TODO
+    //  Expression = Number(value:Double) + Variable(name:String)                      
+    //               + Plus(left:Expression, right:Expression)
+    //               + Times(left:Expression, right:Expression)
+    //   
     
     /**
      * Parse an expression.
+     * 
      * @param input expression to parse, as defined in the PS3 handout.
      * @return expression AST for the input
      * @throws IllegalArgumentException if the expression is invalid
      */
-    public static Expression parse(String input) {
-        throw new RuntimeException("unimplemented");
+    public static Expression parse(String input) throws IllegalArgumentException {
+        try {
+            // Create a stream of characters from the string
+            CharStream stream = new ANTLRInputStream(input);
+            // Make a lexer.
+            ExpressionLexer lexer = new ExpressionLexer(stream);
+            lexer.reportErrorsAsExceptions();
+            TokenStream tokens = new CommonTokenStream(lexer);
+            // Make a parser
+            ExpressionParser parser = new ExpressionParser(tokens);
+            parser.reportErrorsAsExceptions();
+            // Generate the parse tree using the starter rule.
+            ParseTree tree = parser.root();
+            // Make an Expression
+            MakeExpression exprMaker = new MakeExpression();
+            new ParseTreeWalker().walk(exprMaker, tree);
+            
+            return exprMaker.getExpression();
+        } catch (Exception e) {
+            throw new IllegalArgumentException("illegal expression");
+        }
     }
     
     /**
@@ -52,6 +87,37 @@ public interface Expression {
     @Override
     public int hashCode();
     
-    // TODO more instance methods
+    /**
+     * Differentiate this expression with respect to a variable.
+     * 
+     * @param variable the variable to differentiate by, a case-sensitive nonempty string of letters.
+     * @return expression's derivative with respect to variable.
+     */
+    public Expression differentiate(String variable);
     
+    /**
+     * Simplify this expression.
+     * 
+     * @param environment maps variables to values.  Variables are required to be case-sensitive nonempty 
+     *         strings of letters.  The set of variables in environment is allowed to be different than the 
+     *         set of variables actually found in expression.  Values must be nonnegative numbers.
+     * @return an expression equal to the input, but after substituting every variable v that appears in both
+     *         the expression and the environment with its value, environment.get(v).  If there are no
+     *         variables left in this expression after substitution, it must be evaluated to a single number.
+     */
+    public Expression simplify(Map<String,Double> environment);
+    
+    /**
+     * Check if this expression is a Number expression
+     * 
+     * @return true iff this expression is a Number
+     */
+    public boolean isNumber();
+    
+    /**
+     * Get value of this expression
+     * 
+     * @return value of this expression, it must be a Number expression 
+     */
+    public Double getValue();
 }
